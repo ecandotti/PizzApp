44a4c25f697b292717855df6982881daf80efaa4
ecandotti <enzo.candotti16@hotmail.com>
2020-12-04 02:15:15 +0100

    Add Home component

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+    const MainStyle = {
+        marginTop: '10vh',
+        marginBottom: '10vh'
+    }
+    return(
+        <div className="flex-1 flex flex-col flex-nowrap h-full" style={MainStyle}>
+            <p>PizzApp</p>
+        </div>
+    )
+}
+
+export default Main
