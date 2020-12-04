3264d7a2c3757a76da05192c05bf425836625cee
ecandotti <enzo.candotti16@hotmail.com>
2020-12-04 04:44:47 +0100

    MainBoard idea update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { GiOpenTreasureChest } from 'react-icons/gi'
+
 const Main = () => {
     const MainStyle = {
         marginTop: '10vh',
@@ -7,7 +9,11 @@ const Main = () => {
     }
     return(
         <div className="flex-1 flex flex-col flex-nowrap h-full" style={MainStyle}>
-            <p>PizzApp</p>
+            <h2>Bienvenue, <u>User1</u></h2>
+            <p>Nombre de livraisons : 8</p>
+            <p>Contributions ajout : 5</p>
+            <p>A venir</p>
+            <div className="flex flex-row flex-nowrap items-center"><div className="border bg-indigo-900 rounded-full p-2 text-white mx-auto cursor-pointer">Compter sa caisse <GiOpenTreasureChest className="mx-auto"/></div></div>
         </div>
     )
 }
