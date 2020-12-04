6be785f023beb8b1b5d00ba87f765d21941adb3c
ecandotti <enzo.candotti16@hotmail.com>
2020-12-04 04:43:32 +0100

    update adding + chnage redirection after submit

--- src/components/Adding.jsx
+++ src/components/Adding.jsx
@@ -7,7 +7,7 @@ class Adding extends React.Component {
         this.state = {
             nom: '',
             address: '',
-            info: ' '
+            info: ''
         }
     }
 
@@ -24,7 +24,7 @@ class Adding extends React.Component {
         const address = this.state.address
         const info = this.state.info
 
-        const client = { 'cli_name': name, 'cli_address': address, 'cli_info': info }
+        const client = { 'cli_name': name, 'cli_address': address, 'cli_info': info + ' ' }
 
         fetch('https://cors-anywhere.herokuapp.com/http://176.189.0.162:8181/clients/add', {
             method: 'POST',
@@ -37,36 +37,36 @@ class Adding extends React.Component {
             this.setState({
                     name: '',
                     address: '',
-                    info: ' '
+                    info: ''
             })
             //this.props.updateData()
-            window.location.replace("/listclient")
+            window.location.replace("/search")
         })
         .catch(err => console.log(err))
     }
 
     render(){
-        const AddClientStyle = {
+        const AddingStyle = {
             marginTop: '10vh',
             marginBottom: '10vh'
         }
         return(
-            <div className="flex-1 flex flex-col flex-nowrap w-full" style={AddClientStyle}>
+            <div className="flex-1 flex flex-col flex-nowrap w-full" style={AddingStyle}>
                 <form method="POST" className="flex flex-col flex-nowrap items-center w-full p-4" onSubmit={this.handleSubmit}>
                     <div className="my-4">
                         Nom :<br/>
-                        <input name="nom" type="text" className="rounded" value={this.state.nom} onChange={this.handleChange} required/>
+                        <input name="nom" type="text" className="rounded focus:outline-none pl-1" value={this.state.nom} onChange={this.handleChange} required/>
                     </div>
                     <div className="my-4">
                         Adresse :<br/>
-                        <input name="address" type="text" className="rounded" value={this.state.address} onChange={this.handleChange} required/>
+                        <input name="address" type="text" className="rounded focus:outline-none pl-1" value={this.state.address} onChange={this.handleChange} required/>
                     </div>
                     <div className="my-4">
                         Info :<br/>
-                        <input name="info" type="text" className="rounded" value={this.state.info} onChange={this.handleChange} required/>
+                        <input name="info" type="text" className="rounded focus:outline-none pl-1" value={this.state.info} onChange={this.handleChange} required/>
                     </div>
                     <div className="my-4 flex flex-row flex-nowrap items-center">
-                        <button type="submit" className="border bg-green-400 rounded p-4 text-white">+ Ajouter</button>
+                        <button type="submit" className="border bg-green-400 rounded-full py-2 px-4 text-white select-none">+ Ajouter</button>
                     </div>
                 </form>
             </div>
