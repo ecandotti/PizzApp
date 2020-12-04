61f04c0656f66d38ccc6ca35e9b65ec3849ca440
ecandotti <enzo.candotti16@hotmail.com>
2020-12-04 04:44:16 +0100

    chnage icon + select none property

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import { GiFullPizza } from 'react-icons/gi'
+import { FaPizzaSlice } from 'react-icons/fa'
 
 const Header = ({title}) => {
     const headerStyle = {
         height: '10vh'
     }
     return(
-        <div className="fixed flex-1 w-full bg-blue-300" style={headerStyle}>
-            <div className="flex flex-row flex-nowrap text-gray-800 items-center h-full w-full justify-center text-2xl"><GiFullPizza className="mr-2"/> - {title}</div>
+        <div className="fixed flex-1 w-full bg-blue-300 select-none" style={headerStyle}>
+            <div className="flex flex-row flex-nowrap text-gray-800 items-center h-full w-full justify-center text-2xl"><FaPizzaSlice className="mr-2"/> {title}</div>
         </div>
     )
 }
