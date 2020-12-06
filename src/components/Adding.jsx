b192c1b5987afaf00cc40dfe7dddba37e2367268
ecandotti <enzo.candotti16@hotmail.com>
2020-12-06 16:22:41 +0100

    dark mode + bg-focus on res_logo or cli_logo

--- src/components/Adding.jsx
+++ src/components/Adding.jsx
@@ -24,10 +24,14 @@ class Adding extends React.Component {
             this.setState({
                 isClient: true
             })
+            document.getElementById("residenceID").classList.remove("bg-yellow-700")
+            document.getElementById("clientID").classList.add("bg-blue-700")
         } else if(mode === 'residence'){
             this.setState({
                 isClient: false
             })
+            document.getElementById("clientID").classList.remove("bg-blue-700")
+            document.getElementById("residenceID").classList.add("bg-yellow-700")
         } else {
             console.log('Pas de mode transmis')
         }
@@ -70,10 +74,10 @@ class Adding extends React.Component {
         return(
             <div className="flex-1 flex flex-col flex-nowrap w-full" style={AddingStyle}>
                 <div className="mx-auto flex flex-row flex-nowrap m-4">
-                    <div className="cursor-pointer m-4 hover:bg-blue-200 p-4 rounded"  onClick={() => this.switchContact('client')} >
+                    <div className="cursor-pointer m-4 hover:bg-blue-700 p-4 rounded"  onClick={() => this.switchContact('client')} id="clientID">
                         <FaUserAlt className="text-2xl"/>
                     </div>
-                    <div className="cursor-pointer m-4 hover:bg-green-200 p-4 rounded"  onClick={() => this.switchContact('residence')}>
+                    <div className="cursor-pointer m-4 hover:bg-yellow-700 p-4 rounded"  onClick={() => this.switchContact('residence')} id="residenceID">
                         <FaBuilding className="text-2xl"/>
                     </div>
                 </div>
@@ -81,22 +85,22 @@ class Adding extends React.Component {
                     <div>Fiche {this.state.isClient ? 'Client' : 'Résidence'}</div>
                     <div className="my-4">
                         Nom :<br/>
-                        <input name="nom" type="text" className="rounded focus:outline-none pl-1" value={this.state.nom} onChange={this.handleChange} required/>
+                        <input name="nom" type="text" className="rounded focus:outline-none pl-1 text-black" value={this.state.nom} onChange={this.handleChange} required/>
                     </div>
                     <div className="my-4">
                         Adresse :<br/>
-                        <input name="address" type="text" className="rounded focus:outline-none pl-1" value={this.state.address} onChange={this.handleChange} required/>
+                        <input name="address" type="text" className="rounded focus:outline-none pl-1 text-black" value={this.state.address} onChange={this.handleChange} required/>
                     </div>
                     <div className="my-4">
                         Code :<br/>
-                        <input name="code" type="text" className="rounded focus:outline-none pl-1" value={this.state.code} onChange={this.handleChange}/>
+                        <input name="code" type="text" className="rounded focus:outline-none pl-1 text-black" value={this.state.code} onChange={this.handleChange}/>
                     </div>
                     <div className="my-4">
                         Informations :<br/>
-                        <input name="info" type="text" className="rounded focus:outline-none pl-1" value={this.state.info} onChange={this.handleChange}/>
+                        <input name="info" type="text" className="rounded focus:outline-none pl-1 text-black" value={this.state.info} onChange={this.handleChange}/>
                     </div>
                     <div className="my-4 flex flex-row flex-nowrap items-center">
-                        <button type="submit" className="border bg-green-400 rounded-full py-2 px-4 text-white select-none">+ Ajouter</button>
+                        <button type="submit" className="border bg-green-600 hover:bg-green-500 rounded-full py-2 px-4 select-none">+ Ajouter</button>
                     </div>
                 </form>
             </div>
