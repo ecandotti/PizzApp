c17fd272a90c1fcd3761d5eb22c4910b9e98e6d0
ecandotti <enzo.candotti16@hotmail.com>
2020-12-06 05:13:46 +0100

    Clean code (one req/arr) + add /profile/:id route

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,22 @@ import React from 'react'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import DarkModeButton from './components/DarkModeButton'
 import Navigation from './components/Navigation'
 import Search from './components/Search'
 import Adding from './components/Adding'
 import Header from './components/Header'
 import Main from './components/Main'
+import PersonnalCard from './components/PersonnalCard'
 
 class App extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            clientArray: [],
-            residenceArray: [],
+            mainArr: [],
             title: ''
         }
-        this.updateData = this.updateData.bind(this)
         this.changeTitle = this.changeTitle.bind(this)
-    }
-
-    updateData() {
-        fetch('https://cors-anywhere.herokuapp.com/http://176.189.0.162:8181/clients/list')
-            .then(response => response.json())
-            .then(req => {
-                this.setState({
-                clientArray: req
-                })
-            })
-            .then(console.log(this.state.clientArray))
-
-        fetch('https://cors-anywhere.herokuapp.com/http://176.189.0.162:8181/residences/list')
-            .then(response => response.json())
-            .then(req => {
-                this.setState({
-                residenceArray: req
-                })
-            })
-            .then(console.log(this.state.residenceArray))
-    }
-
-    componentDidMount() {
-        this.updateData()
-        this.setState({ title: 'Accueil' })
+        this.updateData = this.updateData.bind(this)
     }
 
     changeTitle(reqTitle) {
@@ -52,24 +26,39 @@ class App extends React.Component {
         })
     }
 
+    async updateData() {
+        let response = await fetch('https://pizzapifire.herokuapp.com/contacts/list')
+        let req = await response.json()
+        this.setState({
+            mainArr: req
+        })
+    }
+
+    componentDidMount(){
+        this.updateData().then(() => { console.log('Data updated !') }).catch(err => {console.log(err)})
+        console.log('Data updating...')
+    }
+
     render(){
         return (
             <div className="flex flex-col flex-nowrap bg-gray-100 dark:bg-gray-800 w-screen h-screen">
-                <DarkModeButton className="fixed"/>
                 <Router>
                     <Header  title={this.state.title}/>
                     <Switch>
                         <Route exact path="/">
-                            <Main />
+                            <Main changeTitle={this.changeTitle}/>
                         </Route>  
                         <Route path="/adding">
-                            <Adding updateData={this.updateData}/>
+                            <Adding updateData={this.updateData} changeTitle={this.changeTitle}/>
                         </Route>
                         <Route path="/search">
-                            <Search clientArray={this.state.clientArray} residenceArray={this.state.residenceArray}/>
+                            <Search updateData={this.updateData} mainArr={this.state.mainArr} changeTitle={this.changeTitle}/>
+                        </Route>
+                        <Route path="/profile/:_id">
+                            <PersonnalCard mainArr={this.state.mainArr}/>
                         </Route>
                     </Switch>
-                    <Navigation title={this.state.title} changeTitle={this.changeTitle}/>
+                    <Navigation title={this.state.title} changeTitle={this.changeTitle} />
                 </Router>
             </div>
         )
