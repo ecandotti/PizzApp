c36392db2ad2c1f7ec5d293de7427a7a51849720
ecandotti <enzo.candotti16@hotmail.com>
2020-12-06 05:14:26 +0100

    clean, one simple req with single form

--- src/components/Adding.jsx
+++ src/components/Adding.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
-import client_pdp from '../img/client.png'
-import residence_pdp from '../img/residence.png'
+import { FaUserAlt, FaBuilding } from 'react-icons/fa'
 
 class Adding extends React.Component {
     constructor(props) {
@@ -10,28 +9,44 @@ class Adding extends React.Component {
         this.state = {
             nom: '',
             address: '',
+            code: '',
             info: ''
         }
     }
-
     handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         })
     }
 
-    handleSubmit = (event) => {
+    switchContact(mode){
+        if(mode === 'client'){
+            this.setState({
+                isClient: true
+            })
+        } else if(mode === 'residence'){
+            this.setState({
+                isClient: false
+            })
+        } else {
+            console.log('Pas de mode transmis')
+        }
+    }
+
+    onSubmit = (event) => {
         event.preventDefault()
 
         const name = this.state.nom
         const address = this.state.address
+        const code = this.state.code
         const info = this.state.info
+        const isClient = this.state.isClient
 
-        const client = { 'cli_name': name, 'cli_address': address, 'cli_info': info + ' ' }
+        const contact = {'isClient': isClient, 'name': name, 'address': address, 'code': code, 'info': info + ' ' }
 
-        fetch('https://cors-anywhere.herokuapp.com/http://176.189.0.162:8181/clients/add', {
+        fetch('https://pizzapifire.herokuapp.com/contacts/add', {
             method: 'POST',
-            body: JSON.stringify(client),
+            body: JSON.stringify(contact),
             headers: {
                 'Content-Type': 'application/json'
             }})
@@ -54,8 +69,16 @@ class Adding extends React.Component {
         }
         return(
             <div className="flex-1 flex flex-col flex-nowrap w-full" style={AddingStyle}>
-                <div className="flex flex-row flex-nowrap items-center m-4"><img src={client_pdp} alt="descriptive" className="w-2/12 mx-auto border rounded cursor-pointer"/><img src={residence_pdp} alt="descriptive" className="w-2/12 mx-auto border rounded cursor-pointer"/></div>
-                <form method="POST" className="flex flex-col flex-nowrap items-center w-full px-4" onSubmit={this.handleSubmit}>
+                <div className="mx-auto flex flex-row flex-nowrap m-4">
+                    <div className="cursor-pointer m-4 hover:bg-blue-200 p-4 rounded"  onClick={() => this.switchContact('client')} >
+                        <FaUserAlt className="text-2xl"/>
+                    </div>
+                    <div className="cursor-pointer m-4 hover:bg-green-200 p-4 rounded"  onClick={() => this.switchContact('residence')}>
+                        <FaBuilding className="text-2xl"/>
+                    </div>
+                </div>
+                <form method="POST" className="flex flex-col flex-nowrap items-center w-full px-4" onSubmit={this.onSubmit}>
+                    <div>Fiche {this.state.isClient ? 'Client' : 'Résidence'}</div>
                     <div className="my-4">
                         Nom :<br/>
                         <input name="nom" type="text" className="rounded focus:outline-none pl-1" value={this.state.nom} onChange={this.handleChange} required/>
@@ -65,8 +88,12 @@ class Adding extends React.Component {
                         <input name="address" type="text" className="rounded focus:outline-none pl-1" value={this.state.address} onChange={this.handleChange} required/>
                     </div>
                     <div className="my-4">
-                        Info :<br/>
-                        <input name="info" type="text" className="rounded focus:outline-none pl-1" value={this.state.info} onChange={this.handleChange} required/>
+                        Code :<br/>
+                        <input name="code" type="text" className="rounded focus:outline-none pl-1" value={this.state.code} onChange={this.handleChange}/>
+                    </div>
+                    <div className="my-4">
+                        Informations :<br/>
+                        <input name="info" type="text" className="rounded focus:outline-none pl-1" value={this.state.info} onChange={this.handleChange}/>
                     </div>
                     <div className="my-4 flex flex-row flex-nowrap items-center">
                         <button type="submit" className="border bg-green-400 rounded-full py-2 px-4 text-white select-none">+ Ajouter</button>
