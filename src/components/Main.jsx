166812f17dea87555f448f56e1a531343bccf7e3
ecandotti <enzo.candotti16@hotmail.com>
2020-12-06 05:16:06 +0100

    update Main (no fonctionnal)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 import { GiOpenTreasureChest } from 'react-icons/gi'
 
+import logo from '../img/pizza.png'
+
 const Main = () => {
+
     const MainStyle = {
         marginTop: '10vh',
         marginBottom: '10vh'
     }
     return(
-        <div className="flex-1 flex flex-col flex-nowrap h-full" style={MainStyle}>
+        <div className="flex-1 flex flex-col flex-nowrap h-full mx-auto p-8 space-y-8" style={MainStyle}>
+            <img src={logo} alt="Logo"/>
             <h2>Bienvenue, <u>User1</u></h2>
-            <p>Nombre de livraisons : 8</p>
-            <p>Contributions ajout : 5</p>
-            <p>A venir</p>
-            <div className="flex flex-row flex-nowrap items-center"><div className="border bg-indigo-900 rounded-full p-2 text-white mx-auto cursor-pointer">Compter sa caisse <GiOpenTreasureChest className="mx-auto"/></div></div>
+            <div className="flex flex-row flex-nowrap items-center"><div className="border bg-indigo-900 rounded-full p-2 text-white mx-auto cursor-not-allowed">Compter sa caisse <GiOpenTreasureChest className="mx-auto"/></div></div>
         </div>
     )
 }
