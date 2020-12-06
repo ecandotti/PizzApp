d3e77865c0905454064128864d8b35a027056dfe
ecandotti <enzo.candotti16@hotmail.com>
2020-12-06 05:15:08 +0100

    Clean Card button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,16 @@
 import React from 'react'
 
-//import {AiFillDelete, AiFillEdit} from 'react-icons/ai'
-/*
-    <div className="flex-1 flex flex-row flex-nowrap w-full">
-        <AiFillDelete className="border h-full bg-red-500 text-white rounded"/>
-        <AiFillEdit className="border h-full bg-yellow-500 text-white rounded"/>
-    </div> 
-*/
+import { FaIdCardAlt } from 'react-icons/fa'
 
 import client_pdp from '../img/client.png'
 import residence_pdp from '../img/residence.png'
 
 const Card = (props) => {
     return(
-        <div className="flex-1 flex flex-nowrap flex-row border p-2 mx-2 mt-2 bg-white rounded h-full">
+        <div className="flex-1 flex flex-nowrap flex-row border p-2 mx-2 mt-2 bg-white rounded-xl h-full items-center cursor-pointer hover:bg-gray-100" onClick={() => props.onClick()}>
             <div className="flex-1"><img src={props.isClient ? client_pdp : residence_pdp} alt="descriptive" className="w-1/2"/></div>
             <div className="flex-1">
-                {props.name}<br/>
-                {props.address}
-            </div>
-            <div className="flex-1">
-                {props.info}TEST
+                <div className="flex flex-row flex-nowrap items-center sm:text-lg"><FaIdCardAlt className="m-2"/> {props.name}</div>
             </div>
         </div>
     )
