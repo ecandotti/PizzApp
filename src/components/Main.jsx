3aa401741710c8b0d260c2a0aae946a163634dbf
ecandotti <enzo.candotti16@hotmail.com>
2020-12-06 16:23:13 +0100

    fix height to 80vh to patch no occuped zone

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,10 +8,11 @@ const Main = () => {
 
     const MainStyle = {
         marginTop: '10vh',
-        marginBottom: '10vh'
+        marginBottom: '10vh',
+        height: '80vh'
     }
     return(
-        <div className="flex-1 flex flex-col flex-nowrap h-full mx-auto p-8 space-y-8" style={MainStyle}>
+        <div className="flex flex-col flex-nowrap h-full mx-auto p-8 space-y-8 h-full" style={MainStyle}>
             <img src={logo} alt="Logo"/>
             <h2>Bienvenue, <u>User1</u></h2>
             <div className="flex flex-row flex-nowrap items-center"><div className="border bg-indigo-900 rounded-full p-2 text-white mx-auto cursor-not-allowed">Compter sa caisse <GiOpenTreasureChest className="mx-auto"/></div></div>
